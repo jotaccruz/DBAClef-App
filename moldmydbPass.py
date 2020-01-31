585991572be280bc8a8de0061ffe8f30f8ee0834
JUAN CARLOS Cruz <juancs02011@gmail.com>
2020-01-31 10:16:37 -0600

    Renaming it
    App re-name

---
+++ moldmydbPass.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 16:14:47 2019
+
+@author: juan.cruz2
+"""
+
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import simpledialog
+
+
+root = tkinter.Tk() # dialog needs a root window, or will create an "ugly" one for you
+root.withdraw() # hide the root window
+
+root.destroy() # clean up after yourself!
